7d4a8f91ffcc57eb1aff16e91ae40c64a88d428f
Kieran Williams <kieranmarcus@me.com>
2021-08-27 12:04:10 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const scrollToDownload = () => {
     scroll.scrollTo(window.innerHeight * 2);
   };
-  let content = "width=device-width, initial-scale=0.6";
+  let content = "width=device-width, initial-scale=1.0";
   return (
     <>
       <Head>
